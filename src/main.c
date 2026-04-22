af4d84533ef4d467156c4adb3e9dc420a0d816d9
mohamedayari1 <ayarim781@gmail.com>
2026-04-22 20:18:24 +0100

    Refactor data types to use uint32_t for consistency and clarity

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-
+#include <stdint.h>
+#include <sys/types.h>
 
 typedef struct {
     char* buffer;
@@ -33,45 +33,50 @@ typedef enum { EXECUTE_SUCCESS,
 } ExecuteResult;
 
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+
 typedef struct {
     Row row_to_insert;
     StatementType type;
 } Statement;
 
 
-#define COLUMN_USERNAME_SIZE 32
-#define COLUMN_EMAIL_SIZE 255
-typedef struct {
-    u_int32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
-} Row;
-
-
-
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
 
-const u_int32_t ID_SIZE = size_of_attribute(Row, id);
-const u_int32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const u_int32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const u_int32_t ID_OFFSET = 0;
-const u_int32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const u_int32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const u_int32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 
-const u_int32_t PAGE_SIZE = 4096;
+const uint32_t PAGE_SIZE = 4096;
 #define TABLE_MAX_PAGES 100
 
-const u_int32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-const u_int32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 
 typedef struct {
-    u_int32_t num_rows;
+    uint32_t num_rows;
     void* pages[TABLE_MAX_PAGES];
 } Table;
+
+
+InputBuffer* new_input_buffer();
+void print_prompt();
+void read_input(InputBuffer* input_buffer);
+void close_input_buffer(InputBuffer* input_buffer);
 
 
 void serialize_row(Row* source, void* destination) {
@@ -87,29 +92,29 @@ void deserialize_row(void* source, Row* destination) {
 }
 
 
-void* row_slot(Table* table, u_int32_t row_num) {
-    u_int32_t page_num = row_num / ROWS_PER_PAGE;
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
     void* page = table->pages[page_num];
     if (page == NULL) {
         // Allocate memory only when we try to access the page
         page = table->pages[page_num] = malloc(PAGE_SIZE);
     }
-    u_int32_t row_offset = row_num % ROWS_PER_PAGE;
-    u_int32_t byte_offset = row_offset * ROW_SIZE;
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
     return page + byte_offset;
 }
 
 Table* new_table() {
     Table* table = malloc(sizeof(Table));
     table->num_rows = 0;
-    for (u_int32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
         table->pages[i] = NULL;
     }
     return table;
 }
 
 void free_table(Table* table) {
-    for (u_int32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
         if (table->pages[i]) {
             free(table->pages[i]);
         }
@@ -163,7 +168,7 @@ ExecuteResult  execute_insert(Statement* statement, Table* table) {
 
 ExecuteResult  execute_select(Statement* statement, Table* table) {
     Row row;
-    for (u_int32_t i = 0; i < table->num_rows; i++) {
+    for (uint32_t i = 0; i < table->num_rows; i++) {
         deserialize_row(row_slot(table, i), &row);
         printf("(%d, %s, %s)\n", row.id, row.username, row.email);
         // print_row(&row);
